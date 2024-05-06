f309f2e514530dcf1f9f05cd5d28d79d4028535e
Henrik Nilsson Harnert <hello@henriknh.com>
2024-05-06 22:27:29 +0200

    Fix sitemap

--- app/sitemap.xml/page.ts
+++ app/sitemap.xml/page.ts
@@ -1,57 +1,30 @@
-//pages/sitemap.xml.js
+import { MetadataRoute } from 'next'
+
 const EXTERNAL_URL = process.env.EXTERNAL_URL as string
 
 if (!EXTERNAL_URL) {
   throw 'EXTERNAL_URL missing'
 }
 
-function generateSiteMap(posts) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>${EXTERNAL_URL}</loc>
-     </url>
-     <url>
-       <loc>${EXTERNAL_URL}/tos</loc>
-     </url>
-     <url>
-       <loc>${EXTERNAL_URL}/privacy-policy</loc>
-     </url>
-     ${posts
-       .map(({ id }) => {
-         return `
-       <url>
-           <loc>${`${EXTERNAL_URL}/${id}`}</loc>
-       </url>
-     `
-       })
-       .join('')}
-   </urlset>
- `
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${EXTERNAL_URL}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${EXTERNAL_URL}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${EXTERNAL_URL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
 }
-
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
-
-export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  // const request = await fetch(EXTERNAL_URL);
-  // const posts = await request.json();
-  const posts = []
-
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts)
-
-  res.setHeader('Content-Type', 'text/xml')
-  // we send the XML to the browser
-  res.write(sitemap)
-  res.end()
-
-  return {
-    props: {},
-  }
-}
-
-export default SiteMap
