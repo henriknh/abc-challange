f94ab89105caf8e15f37c20a73d03ab53ac73eb8
Henrik Nilsson Harnert <hello@henriknh.com>
2024-05-07 09:04:40 +0200

    Create-recipe page renders

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import AuthGuard from '../../components/auth-guard'
 import LetsCook from '../../components/lets-cook'
 import Recipe from '../../components/recipe/recipe'
 import Section from '../../components/section'
@@ -11,35 +10,33 @@ export default function CreateRecipe() {
   const [recipe, formAction] = useFormState(onCook)
 
   return (
-    <AuthGuard>
-      <div className="prose max-w-none pb-10">
-        <Section>
-          <h1>Create new recipe</h1>
+    <div className="prose max-w-none pb-10">
+      <Section>
+        <h1>Create new recipe</h1>
 
-          <div className="flex flex-col gap-10">
-            {/* <LetsCook onAction={formAction} /> */}
+        <div className="flex flex-col gap-10">
+          <LetsCook onAction={formAction} />
 
-            {recipe ? (
-              <div className="flex flex-col gap-10">
-                <div className="rounded-xl border p-10">
-                  <Recipe recipe={recipe} />
-                </div>
-
-                <div className="flex justify-end">
-                  <button className="btn btn-primary" disabled={!recipe}>
-                    Add to my collection
-                  </button>
-                </div>
+          {recipe ? (
+            <div className="flex flex-col gap-10">
+              <div className="rounded-xl border p-10">
+                <Recipe recipe={recipe} />
               </div>
-            ) : (
-              <div className="flex flex-col gap-10">
-                <h2>Welcome to the recipe generator!</h2>
-                <p>To get started simply do it! TODO :)</p>
+
+              <div className="flex justify-end">
+                <button className="btn btn-primary" disabled={!recipe}>
+                  Add to my collection
+                </button>
               </div>
-            )}
-          </div>
-        </Section>
-      </div>
-    </AuthGuard>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-10">
+              <h2>Welcome to the recipe generator!</h2>
+              <p>To get started simply do it! TODO :)</p>
+            </div>
+          )}
+        </div>
+      </Section>
+    </div>
   )
 }
