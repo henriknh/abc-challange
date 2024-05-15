18d158a21d2462491595d86fb640567e1a822c8b
Henrik Nilsson Harnert <hello@henriknh.com>
2024-05-15 17:58:44 +0200

    Parse url with GPT instead of Puppeteer

--- app/api/cook.ts
+++ app/api/cook.ts
@@ -1,71 +1,7 @@
 'use server'
 
 import isValidHttpUrl from '../../utils/is-valid-http-url'
-import { getAssistantGenerateRecipeByContent, openai } from '../../utils/openai'
-import { TextContentBlock } from 'openai/resources/beta/threads/messages'
-import puppeteer from 'puppeteer'
-
-const generateRecipeByIngredients = async (
-  ingredients: string,
-  type?: string,
-  portions?: number
-): Promise<object> => {
-  console.log('handleIngredients', ingredients, type, portions)
-
-  return {}
-}
-
-const extractContentByRL = async (url: string) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
-  await page.goto(url)
-  await page.emulateMediaType('screen')
-  const pageSourceHTML = await page.content()
-
-  await browser.close()
-
-  const regexScriptTags = /<script.*?>.*?<\/script>/gims
-  const noScriptTags = pageSourceHTML.replace(regexScriptTags, '')
-
-  const regexStyleTags = /<style.*?>.*?<\/style>/gims
-  const noStyleTags = noScriptTags.replace(regexStyleTags, '')
-
-  const regexHTMLTags =
-    /<(a|g|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|data|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1|h2|h3|h4|h5|h6|head|header|hgroup|hr|html|i|iframe|img|input|ins|kbd|label|legend|li|link|main|map|mark|menu|meta|meter|nav|noframes|noscript|object|ol|optgroup|option|output|p|param|picture|pre|progress|q|rp|rt|ruby|s|samp|script|search|section|select|small|source|span|strike|strong|style|sub|summary|sup|svg|table|tbody|td|template|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr)[^>]+>/gims
-  const cleanDivTags = noStyleTags.replace(regexHTMLTags, '<$1>')
-
-  const regexHTMLComments = /<!--(.*?)-->/gims
-  const noHTMLComments = cleanDivTags.replace(regexHTMLComments, '')
-
-  const regexOpeningTag = /<.*?>/gims
-  const noOpeningTags = noHTMLComments.replace(regexOpeningTag, '')
-
-  const regexClosingTags = /<\/.*?>/gims
-  const noClosingTags = noOpeningTags.replace(regexClosingTags, '')
-
-  const regexNewLines = /(\r\n|\n|\r)/gims
-  const noNewLines = noClosingTags.replace(regexNewLines, '')
-
-  const regexWhiteSpace = /\s\s+/gims
-  const noWhiteSpace = noNewLines.replace(regexWhiteSpace, ' ')
-
-  console.log('pageSourceHTML', pageSourceHTML.length)
-  console.log('noScriptTags', noScriptTags.length)
-  console.log('noStyleTags', noStyleTags.length)
-  console.log('cleanDivTags', cleanDivTags.length)
-  console.log('noHTMLComments', noHTMLComments.length)
-  console.log('noOpeningTags', noOpeningTags.length)
-  console.log('noClosingTags', noClosingTags.length)
-  console.log('noNewLines', noNewLines.length)
-  console.log('noWhiteSpace', noWhiteSpace.length)
-  console.log(
-    'Reduction:',
-    ((noWhiteSpace.length / pageSourceHTML.length) * 100).toFixed(1) + '%'
-  )
-
-  return noWhiteSpace
-}
+import { openai } from '../../utils/openai'
 
 export interface Unit {
   value: number
@@ -127,43 +63,35 @@ const recipeFormat = {
   ],
 }
 
-const generateRecipeByContent = async (content): Promise<object> => {
-  const assistant = await getAssistantGenerateRecipeByContent()
+const generateRecipeByUrl = async (url) => {
+  const content =
+    `Summarize this recipe, ${url}, in exact this JSON format: ` +
+    JSON.stringify(recipeFormat)
 
-  let run = await openai.beta.threads.createAndRun({
-    assistant_id: assistant.id,
-    thread: {
-      messages: [
-        {
-          role: 'assistant',
-          content:
-            'You should always return the response formatted like this JSON object: ' +
-            JSON.stringify(recipeFormat),
-        },
-        { role: 'user', content },
-      ],
-    },
+  console.log(JSON.stringify(content))
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'user',
+        content,
+      },
+    ],
+    model: 'gpt-4o',
+    response_format: { type: 'json_object' },
   })
 
-  while (run.status !== 'completed') {
-    console.log('Run status:', run.status)
-    await new Promise((r) => setTimeout(r, 500))
-    run = await openai.beta.threads.runs.retrieve(run.thread_id, run.id)
-  }
+  return JSON.parse(completion.choices[0].message.content)
+}
 
-  console.log('Run status:', run.status)
+const generateRecipeByIngredients = async (
+  ingredients: string,
+  type?: string,
+  portions?: number
+): Promise<object> => {
+  console.log('handleIngredients', ingredients, type, portions)
 
-  const threadMessages = await openai.beta.threads.messages.list(run.thread_id)
-
-  const recipe = JSON.parse(
-    (threadMessages.data[0].content[0] as TextContentBlock).text.value
-  )
-
-  recipe.context = console.log('Prompt tokens:', run.usage.prompt_tokens)
-  console.log('Completion tokens:', run.usage.completion_tokens)
-  console.log('Total tokens:', run.usage.total_tokens)
-
-  return recipe
+  return {}
 }
 
 export async function onCook(req, formData: FormData) {
@@ -176,9 +104,8 @@ export async function onCook(req, formData: FormData) {
   const isUrl = isValidHttpUrl(context)
   try {
     if (isUrl) {
-      const content = await extractContentByRL(context)
       const recipe = {
-        ...(await generateRecipeByContent(content)),
+        ...(await generateRecipeByUrl(context)),
         context,
       }
 
