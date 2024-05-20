7d2fc615c079f88e50b67a888ee6b09b96095b73
Henrik Nilsson Harnert <henrik.nilsson.harnert@skolon.com>
2024-05-20 20:01:16 +0200

    Read of undefined

--- utils/compare-object-ids.ts
+++ utils/compare-object-ids.ts
@@ -1,8 +1,8 @@
-import mongoose, { Document, ObjectId } from 'mongoose'
+import mongoose, { Document } from 'mongoose';
 
 export default function compareObjectIds(
   object1: mongoose.Types.ObjectId | Document<unknown, {}, any> & any & Required<{ _id: unknown; }>,
   object2: mongoose.Types.ObjectId | Document<unknown, {}, any> & any & Required<{ _id: unknown; }>
 ): boolean {
-  return object1.equals(object2)
+  return object1?.equals(object2)
 }
