52df57c86b395b521a253111fa39e8b9e6bd23ad
Henrik Nilsson Harnert <henrik.nilsson.harnert@skolon.com>
2024-05-21 11:37:00 +0200

    Fix client>server>client error

--- app/create-recipe/page.tsx
+++ app/create-recipe/page.tsx
@@ -1,15 +1,10 @@
-'use client'
-
 import Section from '../../components/section'
 import AuthGuard from '@/components/auth-guard'
-import { SubmitButton } from '@/components/form/submit-button'
-import { mdiChefHat } from '@mdi/js'
-import Icon from '@mdi/react'
-import { onCook } from 'app/api/cook'
+import LetsCook from '@/components/lets-cook'
 
 export const maxDuration = 60
 
-export default function CreateRecipe() {
+export default async function CreateRecipe() {
   return (
     <AuthGuard>
       <div className="prose max-w-none">
@@ -22,23 +17,7 @@ export default function CreateRecipe() {
               instructions that simplify the original recipe.
             </div>
 
-            <form action={onCook} className="flex gap-4">
-              <label className="input input-bordered flex w-full items-center gap-2">
-                <input
-                  name="context"
-                  type="text"
-                  className="min-w-0 grow"
-                  placeholder="https://www.some-tasty-recipe.com"
-                />
-              </label>
-
-              <div className="flex flex-col items-center">
-                <SubmitButton>
-                  <Icon path={mdiChefHat} size={0.8} />
-                  Let&apos;s cook
-                </SubmitButton>
-              </div>
-            </form>
+            <LetsCook />
           </div>
         </Section>
       </div>
---
+++ components/lets-cook.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { SubmitButton } from './form/submit-button'
+import { mdiChefHat } from '@mdi/js'
+import Icon from '@mdi/react'
+import { onCook } from 'app/api/cook'
+
+export default function LetsCook() {
+  return (
+    <form action={onCook} className="flex gap-4">
+      <label className="input input-bordered flex w-full items-center gap-2">
+        <input
+          name="context"
+          type="text"
+          className="min-w-0 grow"
+          placeholder="https://www.some-tasty-recipe.com"
+        />
+      </label>
+
+      <div className="flex flex-col items-center">
+        <SubmitButton>
+          <Icon path={mdiChefHat} size={0.8} />
+          Let&apos;s cook
+        </SubmitButton>
+      </div>
+    </form>
+  )
+}
