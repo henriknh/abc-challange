9228ddd609c5bcdfc5dca46fcf710b409ab263db
Henrik Nilsson Harnert <henrik.nilsson.harnert@skolon.com>
2024-05-23 17:10:29 +0200

    Fix delete recipe button

--- components/recipe/delete-button.tsx
+++ components/recipe/delete-button.tsx
@@ -14,30 +14,34 @@ export default function RecipeDeleteButton({
   recipe,
 }: RecipeDeleteButtonProps) {
   const [loading, setLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
 
   return (
     <div>
       <button
         type="button"
         className="btn btn-square btn-ghost"
-        onClick={() =>
-          (document.getElementById('my_modal_1') as HTMLFormElement).showModal()
-        }
+        onClick={() => setShowModal(!showModal)}
       >
         <Icon path={mdiTrashCanOutline} size={1} />
       </button>
 
-      <dialog id="my_modal_1" className="modal">
+      <dialog
+        id="my_modal_1"
+        className={'modal' + (showModal ? ' modal-open' : '')}
+      >
         <div className="modal-box">
           <h3 className="text-lg font-bold">Delete {recipe.title}</h3>
           <p className="py-4">
             Are you sure you want to permanently delete this recipe?
           </p>
           <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn btn-ghost">Close</button>
-            </form>
+            <button
+              className="btn btn-ghost"
+              onClick={() => setShowModal(false)}
+            >
+              Close
+            </button>
             <button
               className="btn btn-error"
               disabled={loading}
@@ -51,7 +55,7 @@ export default function RecipeDeleteButton({
           </div>
         </div>
         <form method="dialog" className="modal-backdrop">
-          <button>close</button>
+          <button onClick={() => setShowModal(false)}>close</button>
         </form>
       </dialog>
     </div>
