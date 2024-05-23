7ff54559f690f0bf968ee4d4bb9aeeb8b19b9ce3
Henrik Nilsson Harnert <henrik.nilsson.harnert@skolon.com>
2024-05-23 16:47:21 +0200

    Do not wrap 1-2-3

--- components/landing-page/3-tutorial-and-video-demo.tsx
+++ components/landing-page/3-tutorial-and-video-demo.tsx
@@ -8,7 +8,7 @@ export default async function LandingPageTutorialAndVideoDemo() {
       <div className="prose max-w-none">
         <div className="flex flex-col gap-10">
           <div className="flex flex-col gap-4">
-            <h3 className="m-0">Creating recipes is as easy as 1-2-3</h3>
+            <h3 className="m-0">Creating recipes is as easy as <span className='text-nowrap'>1-2-3</span></h3>
 
             <div className="flex flex-col gap-10">
               <div className="flex flex-col gap-4 md:flex-row">
