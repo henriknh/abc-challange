f96f447d4e12ae2d7441ce14b08678af983119d7
Henrik Nilsson Harnert <henrik.nilsson.harnert@skolon.com>
2024-05-24 08:42:28 +0200

    Update tokens page

--- app/tokens/page.tsx
+++ app/tokens/page.tsx
@@ -1,4 +1,5 @@
 import AuthGuard from '@/components/auth-guard'
+import Pricing from '@/components/pricing'
 import Section from '@/components/section'
 import { increaseTokens } from 'app/api/increase-tokens'
 
@@ -7,12 +8,18 @@ export default function Tokens() {
     <AuthGuard>
       <div className="prose max-w-none ">
         <Section>
-          <div className="flex flex-col items-center">
-            <form action={increaseTokens}>
-              <button type="submit" className="btn btn-primary btn-lg">
-                Gimme sweet sweet tokens :)))
-              </button>
-            </form>
+          <h1>Tokens</h1>
+
+          <div className="flex flex-col gap-10">
+            <Pricing />
+
+            <div className="flex flex-col items-center">
+              <form action={increaseTokens}>
+                <button type="submit" className="btn btn-primary btn-lg">
+                  Gimme sweet sweet free tokens :)))
+                </button>
+              </form>
+            </div>
           </div>
         </Section>
       </div>
